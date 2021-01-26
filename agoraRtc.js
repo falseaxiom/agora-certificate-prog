d179cf91191b2d61655d805b97347a16952143e0
falseaxiom <mlegaspi@princeton.edu>
2021-01-26 14:25:01 -0500

    buggy fix later pls

---
+++ agoraRtc.js
@@ -0,0 +1,57 @@
+let handlefail = function(err){
+    console.log(err);
+}
+
+function addVideoStream(streamId){
+    let remoteContainer = document.getElementById("remoteStream");
+    let streamDiv = document.createElement("div");
+    streamDiv.id = streamId;
+    streamDiv.style.transform = "rotateY(180deg)";
+    streamDiv.style.height = "250px";
+    remoteContainer.appendChild(streamDiv);
+}
+
+document.getElementById("join").onclick = function () {
+    let channelName = document.getElementsById("channelName").value;
+    let username = document.getElementById("username").value;
+    let appId = "dd95732445fa44c69531d808e34903b5";
+
+    let client = AgoraRTC.createClient({
+        mode: "live",
+        codec: "h264"
+    })
+
+    client.init(appId,() => console.log("AgoraRTC Client Connected"),handlefail
+    )
+
+    client.join(
+        null,
+        channelName,
+        username,
+        () =>{
+            var localStream = AgoraRTC.createStream({
+                video: true,
+                audio: true,
+            })
+
+            localStream.init(function(){
+                localStream.play("SelfStream")
+                console.log(`App id: ${appId}\nChannel id: ${channelName}`)
+                client.publish(localStream)
+            })
+        }
+    )
+
+    client.on("stream-added", function (evt){
+        console.log("Added Stream");
+        client.subscribe(evt.stream,handlefail)
+    })
+
+    client.on("stream-subscribed", function(evt){
+        console.log("Subscribed Stream");
+        let stream = evt.stream;
+        addVideoStream(stream.getId());
+        stream.play(stream.getId());
+    })
+
+}
